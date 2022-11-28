3273ec6f2454a790c468db55b6fe04fbdf23cc09
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-11-28 20:32:46 +0900

    _

--- 알고리즘 정리/KMP문자열매칭.js
+++ 알고리즘 정리/KMP문자열매칭.js
@@ -1,36 +1,35 @@
 function makeTable(str) {
-    const table = new Array(str.length)
-    let j = 0
-    table[0] = 0
-    for (var i = 1; i < str.length; i++) {
-        while (j > 0 && str.charAt(i) !== str.charAt(j)) j = table[j - 1]
-        if (str.charAt(j) === str.charAt(i))  j++
-        table[i] = j
-    }
-    return table
+  const table = new Array(str.length);
+  let j = 0;
+  table[0] = 0;
+  for (var i = 1; i < str.length; i++) {
+    while (j > 0 && str.charAt(i) !== str.charAt(j)) j = table[j - 1];
+    if (str.charAt(j) === str.charAt(i)) j++;
+    table[i] = j;
+  }
+  return table;
 }
 
 function KMP(str, pattern) {
-    const prefixes = makeTable(pattern)
-    const matches = []
+  const prefixes = makeTable(pattern);
+  const matches = [];
 
-    let j = 0
-    let i = 0
-    while (i < str.length) {
-        if (str[i] === pattern[j]) {
-            i++
-            j++
-        }
-        if (j === pattern.length) {
-            matches.push(i-j)
-            j = prefixes[j-1]
-        }
-        else if (str[i] !== pattern[j]) {
-            if (j !== 0) j = prefixes[j-1]
-            else i++
-        }
+  let j = 0;
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] === pattern[j]) {
+      i++;
+      j++;
     }
-    return matches
+    if (j === pattern.length) {
+      matches.push(i - j);
+      j = prefixes[j - 1];
+    } else if (str[i] !== pattern[j]) {
+      if (j !== 0) j = prefixes[j - 1];
+      else i++;
+    }
+  }
+  return matches;
 }
 
-console.log(KMP('aabcdabcdabcdaaa','abcdab'))
+console.log(KMP("aabcdabcdabcdaaa", "abcdab"));
--- 알고리즘 정리/라빈카프문자열매칭.js
+++ 알고리즘 정리/라빈카프문자열매칭.js
@@ -1,34 +1,34 @@
 const isStringEqual = function (firstString, secondString) {
-    if (firstString !== secondString) return false
-    for (let i = 0; i < firstString.length; i++) {
-        if (firstString[i] !== secondString[i]) return false
-    }
-    return true
-}
+  if (firstString !== secondString) return false;
+  for (let i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== secondString[i]) return false;
+  }
+  return true;
+};
 
 const calcHash = function (myText, largePrime, randomNumber) {
-    let hash = 0
-    for (let i = 0; i < myText.length; i++) {
-        hash = (hash * randomNumber + myText.charCodeAt(i)) % largePrime
-    }
-    return hash
-}
+  let hash = 0;
+  for (let i = 0; i < myText.length; i++) {
+    hash = (hash * randomNumber + myText.charCodeAt(i)) % largePrime;
+  }
+  return hash;
+};
 
 const rabinKarp = function (parent, pattern) {
-    let largePrime = 337
-    let randomNumber = 50
-    let stringPositions = []
-    let patternHash = calcHash( pattern, largePrime, randomNumber)
-    let text
-    let parentHash
+  let largePrime = 337;
+  let randomNumber = 50;
+  let stringPositions = [];
+  let patternHash = calcHash(pattern, largePrime, randomNumber);
+  let text;
+  let parentHash;
 
-    for (let i = 0; i <= parent.length - pattern.length; i++) {
-        text = parent.slice(i, i + pattern.length)
-        parentHash = calcHash(text, largePrime, randomNumber)
-        if (isStringEqual(text, pattern)) stringPositions.push(i)
-        if (patternHash !== parentHash) continue
-    }
-    return stringPositions
-}
+  for (let i = 0; i <= parent.length - pattern.length; i++) {
+    text = parent.slice(i, i + pattern.length);
+    parentHash = calcHash(text, largePrime, randomNumber);
+    if (isStringEqual(text, pattern)) stringPositions.push(i);
+    if (patternHash !== parentHash) continue;
+  }
+  return stringPositions;
+};
 
-console.log(rabinKarp('aabcdabcdabcdaaa','abcdab'))
+console.log(rabinKarp("aabcdabcdabcdaaa", "abcdab"));
