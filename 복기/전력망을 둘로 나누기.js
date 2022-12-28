bd4c7a1c350a505ae6c3006faab081b7b2866cbb
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-12-28 21:35:31 +0900

    Study / 복기 / 전력망을 둘로 나누기, 빛의경로사이클

--- 복기/전력망을 둘로 나누기.js
+++ 복기/전력망을 둘로 나누기.js
@@ -33,3 +33,71 @@ function solution(n, wires) {
 
   return answer;
 }
+
+let unionFinds = [];
+
+const find = (a) => {
+  if (unionFinds[a] < 0) return a;
+  unionFinds[a] = find(unionFinds[a]);
+  return unionFinds[a];
+};
+
+const merge = (a, b) => {
+  fa = find(a);
+  fb = find(b);
+  if (fa === fb) return;
+
+  unionFinds[fa] += unionFinds[fb];
+  unionFinds[fb] = fa;
+};
+
+function solution(n, wires) {
+  let result = Infinity;
+  const N = wires.length;
+
+  for (let node = 0; node < N; node++) {
+    unionFinds = new Array(n + 1).fill(-1);
+    const graph = wires.filter((_, i) => i !== node);
+
+    for (const [v, w] of graph) merge(v, w);
+
+    const [a, b] = unionFinds.slice(1).filter((v) => v < 0);
+    result = Math.min(result, Math.abs(a - b));
+  }
+
+  return result;
+}
+
+class Tree {
+  constructor(n) {
+    this.tree = new Array(n);
+    this.min = n - 1;
+  }
+  addConnect(v1, v2) {
+    this.connect(v1, v2);
+    this.connect(v2, v1);
+  }
+  connect(v, target) {
+    const connects = this.tree[v - 1] || [];
+    connects.push(target);
+    this.tree[v - 1] = connects;
+  }
+  getCount(v, except) {
+    const connects = this.tree[v - 1] || [];
+    if (connects.length === 1) return 1;
+    return connects.reduce((acc, cur) => {
+      if (cur === except) return acc;
+      return acc + this.getCount(cur, v);
+    }, 1);
+  }
+  setDiff(v1, v2) {
+    const diff = Math.abs(this.getCount(v1, v2) * 2 - this.tree.length);
+    this.min = Math.min(this.min, diff);
+  }
+}
+function solution(n, wires) {
+  const tree = new Tree(n);
+  wires.forEach(([v1, v2]) => tree.addConnect(v1, v2));
+  wires.forEach(([v1, v2]) => tree.setDiff(v1, v2));
+  return tree.min;
+}
