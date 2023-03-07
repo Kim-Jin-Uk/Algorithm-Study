be96068cdfde8b7f156addbf45f2653485b2a2b9
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-03-07 19:01:42 +0900

    Study / 복기 / 섬 연결하기

--- 복기/섬 연결하기.js
+++ 복기/섬 연결하기.js
@@ -1,30 +1,33 @@
-// 섬의 부모를 찾는 함수
-const getParent = (parent, c) => {
-  if (parent[c] === c) return c;
-  return (parent[c] = getParent(parent, parent[c]));
-};
-// 두 섬의 부모를 하나로 합치는 함수
-const unionParent = (parent, c1, c2) => {
-  const [n1, n2] = [getParent(parent, c1), getParent(parent, c2)];
-  if (n1 < n2) return (parent[n2] = n1);
-  else return (parent[n1] = n2);
-};
-// 두 섬의 동일 부모 여부를 조회하는 함수
-const findParent = (parent, c1, c2) => {
-  const [n1, n2] = [getParent(parent, c1), getParent(parent, c2)];
-  return n1 === n2;
-};
-
 const solution = (n, costs) => {
   let answer = 0;
   const parent = new Array(n).fill(null).map((_, i) => i);
-  costs.sort((a, b) => a[2] - b[2]);
+  const rank = new Array(n).fill(0);
 
-  for (const cost of costs) {
-    // 만약 두 섬의 부모가 다른 케이스: 사이클이 형성되지 않은 상태
-    if (!findParent(parent, cost[0], cost[1])) {
-      answer += cost[2]; // 가중치를 더해준다 (정렬했기에 최소값이다)
-      unionParent(parent, cost[0], cost[1]); // 부모를 합쳐준다 (사이클 생성)
+  costs.sort((a, b) => a[2] - b[2]);
+  // 부모를 찾는 함수
+  const findRootFor = (parent, c) => {
+    let root = c;
+    while (root !== parent[root]) root = parent[root];
+    while (c !== root) {
+      const p = parent[c];
+      parent[c] = root; // path compression 기법 적용
+      c = p;
+    }
+    return root;
+  };
+  // 부모를 하나로 합치는 함수
+  const unionChildsFor = (parent, rank, c1, c2) => {
+    const [root1, root2] = [findRootFor(parent, c1), findRootFor(parent, c2)];
+    if (root1 === root2) return;
+    parent[root1] = root2;
+    // union find rank 기법 적용
+    if (rank[root1] === rank[root2]) rank[root1]++;
+  };
+  // 시간 복잡도는 O(nlog*n)
+  for (const c of costs) {
+    if (findRootFor(parent, c[0]) !== findRootFor(parent, c[1])) {
+      answer += c[2];
+      unionChildsFor(parent, rank, c[0], c[1]);
     }
   }
   return answer;
